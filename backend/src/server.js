775d83536eb9eae795dd5bcb27c8a2cb739fa0a9
Danny <dannysteve0693@gmail.com>
2025-11-05 08:55:40 +0700

    Setup Codebase

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { ENV } from './lib/env.js'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: "success from backend" })
+})
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ msg: "API is up and running" })
+})
+
+app.listen(ENV.PORT, () => console.log('Server is running on port:', ENV.PORT))
